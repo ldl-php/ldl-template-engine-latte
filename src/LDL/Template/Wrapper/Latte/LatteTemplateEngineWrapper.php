7fbc0ee59e67e6ee33de9bf838d2b1d5ad188cb8
Federico <jpfstange@gmail.com>
2020-09-25 18:23:51 -0300

    First commit

---
+++ src/LDL/Template/Wrapper/Latte/LatteTemplateEngineWrapper.php
@@ -0,0 +1,61 @@
+<?php declare(strict_types=1);
+
+namespace LDL\Template\Wrapper\Latte;
+
+use Latte\Loaders\StringLoader;
+use LDL\Template\Contracts\TemplateEngineInterface;
+use Latte\Engine as Latte;
+
+class LatteTemplateEngineWrapper implements TemplateEngineInterface
+{
+    /**
+     * @var Latte
+     */
+    private $engine;
+
+    public function __construct(Latte $engine)
+    {
+        $this->engine = $engine;
+    }
+
+    public function renderFromString(string $string, $values): string
+    {
+        $engine = clone $this->engine;
+
+        $loader = new StringLoader(['main' => $string]);
+
+        $engine->setLoader($loader);
+
+        $this->throwOnAnyError();
+
+        $return = $engine->renderToString('main', $values);
+
+        restore_error_handler();
+
+        return $return;
+    }
+
+    public function render(string $template, $values): string
+    {
+        $this->throwOnAnyError();
+        $result = $this->engine->renderToString($template, $values);
+
+        restore_error_handler();
+
+        return $result;
+    }
+
+    private function throwOnAnyError() :void
+    {
+        /*
+         * Throw exceptions on any undefined variable ot whatever it might go wrong
+         */
+        set_error_handler(function ($errno, $errStr, $errFile, $errLine) {
+            if (!(error_reporting() & $errno)) {
+                return false;
+            }
+
+            throw new \RuntimeException("$errno: $errStr | File: $errFile | Line: $errLine");
+        });
+    }
+}
